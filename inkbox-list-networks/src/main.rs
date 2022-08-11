b61e1181e01714e156fda0ca1bb88e0c1ac3eefe
Szybet <53944559+Szybet@users.noreply.github.com>
2022-08-12 00:10:31 +0200

    actually this is better - modularity

--- inkbox-list-networks/src/main.rs
+++ inkbox-list-networks/src/main.rs
@@ -33,68 +33,17 @@ fn main() {
         fs::read_to_string("/opt/inkbox_device").expect("Something went wrong reading the file");
 
     //println!("model is: {}", model);
-
-    let mut WIFI_MODULE: String = String::new();
-    let mut SDIO_WIFI_PWR_MODULE: String = String::new();
-    let mut WIFI_DEV: String = String::new();
-
-    if model == "n873" || model == "n236" || model == "n306" {
-        WIFI_MODULE = "/modules/wifi/8189fs.ko".to_owned();
-        SDIO_WIFI_PWR_MODULE = "/modules/drivers/mmc/card/sdio_wifi_pwr.ko".to_owned();
-        WIFI_DEV = "eth0".to_owned();
-    } else if model == "n705" || model == "n905b" || model == "n905c" || model == "n613" {
-        WIFI_MODULE = "/modules/dhd.ko".to_owned();
-        SDIO_WIFI_PWR_MODULE = "/modules/sdio_wifi_pwr.ko".to_owned();
-        WIFI_DEV = "eth0".to_owned();
-    } else if model == "n437" {
-        WIFI_MODULE = "/modules/wifi/bcmdhd.ko".to_owned();
-        SDIO_WIFI_PWR_MODULE = "/modules/drivers/mmc/card/sdio_wifi_pwr.ko".to_owned();
-        WIFI_DEV = "wlan0".to_owned();
-    } else if model == "kt" {
-        WIFI_MODULE = "ar6003".to_owned();
-        WIFI_DEV = "wlan0".to_owned();
-    } else {
-        WIFI_MODULE = "/modules/dhd.ko".to_owned();
-        SDIO_WIFI_PWR_MODULE = "/modules/sdio_wifi_pwr.ko".to_owned();
-        WIFI_DEV = "eth0".to_owned();
-    }
-    //println!("WIFI_MODULE is: {}", WIFI_MODULE);
-    //println!("SDIO_WIFI_PWR_MODULE is: {}", SDIO_WIFI_PWR_MODULE);
-    //println!("WIFI_DEV is: {}", WIFI_DEV);
-
-    if model != "kt" {
-        insmod(&SDIO_WIFI_PWR_MODULE);
-        insmod(&WIFI_DEV);
-    } else {
-        Command::new("modprobe")
-            .arg("-r")
-            .arg(&WIFI_MODULE)
-            .output()
-            .expect("failed to execute iwlist");
-    }
-    // Race condition
-    thread::sleep(time::Duration::from_millis(1500));
+    /* 
+    Command::new("/usr/local/bin/wifi/toggle.sh")
+    .arg("on")
+    .output()
+    .expect("failed to execute iwlist");
+    */
+    
     if model == "n705" || model == "n905b" || model == "n905c" || model == "n613" || model == "n437"
     {
-        Command::new("wlarm_le")
-            .arg("up")
-            .output()
-            .expect("failed to execute iwlist");
         wlarm_le_scan();
     } else {
-        Command::new("ifconfig")
-            .arg(WIFI_DEV)
-            .arg("up")
-            .output()
-            .expect("failed to execute iwlist");
         iwlist_scan();
     }
-}
-
-pub fn insmod(module: &str) {
-    let new_command = Command::new("insmod")
-        .arg(module)
-        .output()
-        .expect("failed to execute iwlist");
-    //println!("insmod exit status is: {:?}", new_command.status);
 }
